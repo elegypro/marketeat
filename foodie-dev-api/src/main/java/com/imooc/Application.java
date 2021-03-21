a1ba3154ddbe833461de41e3d5abbf1205132d3d
Swimcat <953461624@qq.com>
2021-03-21 19:14:03 +0800

    feat(init): ready eat spring boot

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,8 +2,9 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="controller")
 public class Application {
 
     public static void main(String[] args) {
