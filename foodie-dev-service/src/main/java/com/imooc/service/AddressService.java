c4cf5117ecf38963626cee2446fa1b1831fa706d
Swimcat <953461624@qq.com>
2021-04-01 21:18:11 +0800

    feat(init)User address perfect

--- foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
@@ -1,10 +1,24 @@
 package com.imooc.service;
 
 import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
 
 import java.util.List;
 
 public interface AddressService {
 
+    //根据id查询用户的收货地址
     public List<UserAddress> querAll(String userId);
+
+    //用户新增地址
+    public void addNewUserAddress(AddressBO addressBO);
+
+    //用户修改地址
+    public void updateUserAddress(AddressBO addressBO);
+
+    //用户id和地址id，删除对应的用户地址信息
+    public void deleteUserAddress(String userId,String addressId);
+
+    //修改默认的地址
+    public void updateUserAddressToBeDefault(String userId,String addressId);
 }
