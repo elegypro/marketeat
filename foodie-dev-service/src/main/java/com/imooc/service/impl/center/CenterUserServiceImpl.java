257b622c63967b6285a3a5975e6e603cf9b855c6
Swimcat <953461624@qq.com>
2021-04-12 12:31:20 +0800

    feat(init)usercenter Information complete

---
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/center/CenterUserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.imooc.service.impl.center;
+
+import com.imooc.enums.Sex;
+import com.imooc.mapper.UsersMapper;
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+import com.imooc.pojo.bo.center.CenterUserBO;
+import com.imooc.service.UserService;
+import com.imooc.service.center.CenterUserService;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.MD5Utils;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
+
+@Service
+public class CenterUserServiceImpl implements CenterUserService {
+
+    @Autowired
+    public UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserInfo(String userId) {
+        Users users = usersMapper.selectByPrimaryKey(userId);
+        //密码我们不要带过去
+        users.setPassword(null);
+        return users;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO) {
+
+        Users updateUser = new Users();
+        BeanUtils.copyProperties(centerUserBO,updateUser);
+        updateUser.setId(userId);
+        updateUser.setUpdatedTime(new Date());
+
+        usersMapper.updateByPrimaryKeySelective(updateUser);
+
+        return queryUserInfo(userId);
+    }
+}
+
+
